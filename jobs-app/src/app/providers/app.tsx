d7d5ad2925ff074e183b0f061595106c281bc2ec
Bunbee <barnabas@velocityinc.tech>
2023-09-20 13:41:08 +0200

    Add AppProvider

--- jobs-app/src/app/providers/app.tsx
+++ jobs-app/src/app/providers/app.tsx
@@ -1,1 +1,20 @@
 import { ChakraProvider, GlobalStyle,} from "@chakra-ui/react";
+import { ReactNode } from "react";
+
+import { theme } from '@/config/theme '
+
+
+type AppProviderProps = {
+  children: ReactNode
+}
+
+export const AppProvider = ({
+  children,
+}: AppProviderProps => {
+  return (
+    <ChakraProvider theme={theme}>
+      <GlobalStyle />
+      {children}
+    </ChakraProvider>
+  )
+})
